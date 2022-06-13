a49d1758c0ff2996f5d3f3651f6fc3968180733a
sky-lu <honglin.lu.sr@gmail.com>
2022-06-12 22:23:27 -0400

    Main functionalities using JavaScript

--- main.js
+++ main.js
@@ -1,43 +1,165 @@
+/*
+Main business functionalities
 
+1. click Start:
+    Button: start -> stop;
+    Timer begins to work.
+    The current Lap timer begins to work.
+    Left button is Lap button.
+
+2. click Stop:
+    Button: stop -> start;
+    Button: lap -> reset;
+    Timer and lap timer stop.
+
+3. click Lap:
+    The last lap timer stops working.
+    A new lap timer begins to work.
+    When the lap records >= 2, the max lap record will 
+            be red and the min lap record will be green.
+
+4. click Reset:
+    Reset timer.
+    Lap records are all removed.
+*/
 const timer = document.querySelector(".timer");
-const start_button = document.querySelector(".btn-start");
-const stop_button = document.querySelector(".btn-stop");
-let [minutes, seconds, milliseconds] = [0, 0, 0];
+const startButton = document.querySelector(".btn-start");
+const stopButton = document.querySelector(".btn-stop");
+const lapButton = document.querySelector(".btn-lap");
+const resetButton = document.querySelector(".btn-reset");
+const lapRecords = document.querySelector(".lap-records");
+
+let [timerMinutes, timerSeconds, timerCentiseconds] = [0, 0, 0];
+let [lapMinutes, lapSeconds, lapCentiseconds] = [0, 0, 0];
+
+let li = null;
+let lapTitleSpan = null;
+let lapTimeSpan = null;
+let counter = 1; // to make the Lap title. (eg. Lap 2)
+let lapTimes = []; // change each lap time to centiseconds then put all integers in this array
+let [maxIndex, minIndex] = [-1, -1]; //the index of max lapTime and min lapTime in the array lapTimes
+
+let maxLapLi = null;
+let minLapLi = null;
 
 
 const resetTimer = () => {
     timer.innerHTML = '00:00.00';
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
+    [timerMinutes, timerSeconds, timerCentiseconds] = [0, 0, 0];
 }
 
-const displayTimer = () => {
-    milliseconds = milliseconds + 5;
-    if (milliseconds >= 100) {
-        seconds++;
-        milliseconds = milliseconds - 100;
+const calculateTime = (min, sec, cisec) => {
+    cisec = cisec + 1;
+    if (cisec >= 100) {
+        sec++;
+        cisec = cisec - 100;
     }
-    if (seconds >= 60){
-        minutes++;
-        seconds = seconds -60;
+    if (sec >= 60){
+        min++;
+        sec = sec - 60;
     }
-    timer.innerHTML = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0') 
-                        + '.' + milliseconds.toString().padStart(2, '0');
+    return [min, sec, cisec];
     
 }
 
-let intervalInstance = null;
-start_button.addEventListener('click', () => {
-    intervalInstance = setInterval(displayTimer, 50);
-    start_button.style.display = "none"
-    stop_button.style.display = "block";
+const displayTimer = () => {
+    [timerMinutes, timerSeconds, timerCentiseconds] = calculateTime(timerMinutes, timerSeconds, timerCentiseconds);
+    timer.innerHTML = timerMinutes.toString().padStart(2, '0') + ':' + timerSeconds.toString().padStart(2, '0') 
+                        + '.' + timerCentiseconds.toString().padStart(2, '0');   
+}
+
+const displayLapTime = () => {
+    [lapMinutes, lapSeconds, lapCentiseconds] = calculateTime(lapMinutes, lapSeconds, lapCentiseconds);
+    lapTimeSpan.innerHTML = lapMinutes.toString().padStart(2, '0') + ':' + lapSeconds.toString().padStart(2, '0') 
+    + '.' + lapCentiseconds.toString().padStart(2, '0');   
+    
+}
+
+const createLiElement = () => {
+    li = document.createElement("li");
+    li.setAttribute('id', counter)
+    lapTitleSpan = document.createElement("span");
+    lapTimeSpan = document.createElement("span");
+    lapRecords.prepend(li);
+    li.appendChild(lapTitleSpan);
+    li.appendChild(lapTimeSpan);
+    lapTitleSpan.innerHTML = 'Lap ' + counter;
+}
+
+let timerIntervalInstance = null;
+let lapIntervalInstance = null;
+startButton.addEventListener('click', () => {
+    if (timerMinutes == 0 && timerSeconds == 0 && timerCentiseconds == 0){
+        createLiElement();
+    }
+    timerIntervalInstance = setInterval(displayTimer, 10);
+    lapIntervalInstance = setInterval(displayLapTime, 10);
+    
+    startButton.style.display = "none"
+    stopButton.style.display = "block";
+
+    lapButton.style.display = "block";
+    resetButton.style.display = "none";   
 })
 
 
-stop_button.addEventListener('click', () => {
+stopButton.addEventListener('click', () => { 
+    clearInterval(timerIntervalInstance);
+    clearInterval(lapIntervalInstance);
 
-    clearInterval(intervalInstance);
-    stop_button.style.display = "none"
-    start_button.style.display = "block";
+    stopButton.style.display = "none"
+    startButton.style.display = "block";
+
+    resetButton.style.display = "block";
+    lapButton.style.display = "none";
+    
+})
+
+resetButton.addEventListener('click', () => {
+    resetTimer();
+    lapRecords.innerHTML = "";
+    counter = 1;
+    [lapMinutes, lapSeconds, lapCentiseconds] = [0, 0, 0];
+    lapTimes = [];
+    lapButton.style.display = "block";
+    resetButton.style.display = "none";
+})
+
+const changeLapTimeToCentiseconds = (lapMinutes, lapSeconds, lapCentiseconds) => {
+    return lapMinutes * 60 * 100 + lapSeconds * 100 + lapCentiseconds
+}
+
+
+const compareLapTime = (lapTimes) => {
+    // get the index of max lapTime and min lapTime in the array lapTimes
+    let max = lapTimes.indexOf(Math.max(...lapTimes));
+    let min = lapTimes.indexOf(Math.min(...lapTimes));
+    return [max, min];
+}
+
+const displayMaxMinLapTime = (maxIndex, minIndex) => {
+    maxLapLi = document.getElementById(maxIndex + 1);
+    minLapLi = document.getElementById(minIndex + 1);
+    maxLapLi.style.color = '#d43b36';
+    minLapLi.style.color = '#3db85d';
+}
+
+
+lapButton.addEventListener('click', () => {
+    let lapTime = changeLapTimeToCentiseconds(lapMinutes, lapSeconds, lapCentiseconds);
+    lapTimes.push(lapTime);
+    if(lapTimes.length >= 2){
+        if (maxLapLi && minLapLi){
+            maxLapLi.style.color = '#fefdff';
+            minLapLi.style.color = '#fefdff';
+        }
+        [maxIndex, minIndex] = compareLapTime(lapTimes);
+        displayMaxMinLapTime(maxIndex, minIndex);
+    }
+    counter++;
+    [lapMinutes, lapSeconds, lapCentiseconds] = [0, 0, 0];
+    clearInterval(lapIntervalInstance);
+    createLiElement();
+    lapIntervalInstance = setInterval(displayLapTime, 10);
+
 })
